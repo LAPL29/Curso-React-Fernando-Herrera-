4478c3aed72c082668a5c3d2ba28740e3a8f5289
Luis Ángel Peraza Lugo <perazalugo1@gmail.com>
2023-11-30 14:05:42 -0700

    Avance

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -10,25 +10,37 @@ export const CounterApp = ({ value }) => { //rafc snippet
     //   setCounter(counter + 1);
       setCounter((c) => c+1);
     }
-
+    const handleRest = () => {
+        //   setCounter(counter + 1);
+          setCounter((c) => c-1);
+    }
+    const reset = () => {
+        //   setCounter(counter + 1);
+          setCounter((c) => c=value);
+    }
+ 
     return (
         <>
             <h1>CounterAPP</h1>
             <h2>{counter}</h2>
             <p>{value}</p>
-            <button onClick = {(handleAdd)}>
-                +1
-            </button>
-
+            <button onClick = {(handleAdd)}>+1</button>
+            <button onClick = {(handleRest)}>-1</button>
+            <button onClick = {(reset)}>Reset</button>
+    
         </> 
     )
 }
-
+ 
 CounterApp.propTypes = {
     value: PropTypes.number.isRequired
 }
 
+
 CounterApp.defaultProps = { //Valores por defecto
     value: 'No hay valor'
 }
+
+
+
 
---
+++ 03-counter-app-vite/test/demo.test.js
@@ -0,0 +1,15 @@
+
+test('Esta prueba no debe de fallar', ( ) =>{
+    
+    // 1. Inicialización
+    const message1 = 'Hola Mundo';
+    // 2. Estímulo
+    const message2 = message1.trim(); //  se utiliza para eliminar los espacios en blanco
+    // 3. Observar el comportamiento
+    expect(message1).toBe(message2);
+
+})
+
+
+
+
